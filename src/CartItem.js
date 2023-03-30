626f6e33246d99aad1f8654e36a4697d9534f738
Saurabh <saurabh.koli7420@gmail.com>
2023-03-30 13:32:12 +0530

    Add increaseQuantity Fun

--- src/CartItem.js
+++ src/CartItem.js
@@ -3,23 +3,34 @@ import React from 'react'; //to write the react component
 //Class Component
 class CartItem extends React.Component {
   //create state
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      title:'phone',
-      price:999,
-      qty:1,
-      img:''
+    this.state = {
+      title: 'phone',
+      price: 999,
+      qty: 1,
+      img: ''
     }
+
+    //alternative bind method
+    // this.increaseQuantity=this.increaseQuantity.bind(this);
+  }
+  /*
+  increaseQuantity() {
+    console.log(this.state);
+  }
+  */
+  increaseQuantity=()=> {
+    console.log(this.state);
   }
   render() {
     //js 
-    const {price,title,qty,img} = this.state;
+    const { price, title, qty, img } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
           {/* inline style */}
-          <img alt="Product Image" style={styles.image} src={this.state.img}/>
+          <img alt="Product Image" style={styles.image} src={this.state.img} />
         </div>
         <div className="right-block">
           <div style={{ fontSize: 25 }}>{title}</div>
@@ -27,10 +38,11 @@ class CartItem extends React.Component {
           <div style={{ color: '#777' }}>{qty}</div>
           <div className="cart-item-actions">
             {/* Buttons */}
-              <img alt="decrease" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/992/992514.png"/>
-              <img alt="increase" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"/>
-              <img alt="delete" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/1215/1215092.png"/>
-           
+            <img alt="decrease" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/992/992514.png" />
+            {/* <img onClick={ this.increaseQuantity.bind(this) } alt="increase" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"/> */}
+            <img onClick={this.increaseQuantity} alt="increase" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png" />
+            <img alt="delete" className='action-icons' src="https://cdn-icons-png.flaticon.com/512/1215/1215092.png" />
+
           </div>
         </div>
       </div>
